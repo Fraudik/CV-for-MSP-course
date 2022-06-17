38ebe3d021f8b2f6e821cf8adb72879f486cca0d
Fraudik <52631426+Fraudik@users.noreply.github.com>
2022-06-17 10:05:08 +0300

    Change pdflatex to xelatex

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,21 @@
 #!/bin/sh
 
-cd /CV
-pdflatex -interaction=nonstopmode main.tex
-ls
-cd ..
-mv CV/main.pdf main.pdf
-ls
+cp /etc/apt/sources.list /etc/apt/sources.list.bak && sed -i -re 's/([a-z]{2}\.)?archive.ubuntu.com|security.ubuntu.com/old-releases.ubuntu.com/g' /etc/apt/sources.list && apt-get update && apt-get dist-upgrade && apt-get install --yes --no-install-recommends \
+    cm-super \
+    dvipng \
+    texlive-base \
+    texlive-fonts-extra \
+    texlive-fonts-recommended \
+    texlive-generic-extra \
+    texlive-generic-recommended \
+    texlive-lang-cyrillic \
+    texlive-latex-base \
+    texlive-latex-extra \
+    texlive-latex-recommended \
+    texlive-pictures \
+    texlive-science \
+    biber \
+    texlive-bibtex-extra \
+    texlive-xetex
+
+xelatex -interaction=nonstopmode CV/main.tex
